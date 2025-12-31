114ab77a821f0af6d2d700365436f7d05aa922be
DUDU0123 <sdu200115@gmail.com>
2025-12-31 13:16:40 +0530

    app_assets added

---
+++ lib/utils/constants/app_assets.dart
@@ -0,0 +1,18 @@
+class AppAssets {
+  // app fonts
+  static const String lugfaRegularFont = 'LufgaRegular';
+  static const String lugfaMediumFont = 'LufgaMedium';
+
+  //app images
+  static const String appLoginPageBanner = "assets/images/login_page_banner.png";
+
+  // app icons
+  static const notificationIcon = "assets/images/notification_icon.png";
+  static const searchIcon = "assets/images/search_icon.png";
+  static const profileIcon = "assets/images/profile_icon.png";
+  static const sortIcon= "assets/images/sort_icon.png";
+  static const favIcon = "assets/images/fav_icon.png";
+  static const filterIcon = "assets/images/filter_icon.png";
+  static const cartIcon = "assets/images/cart_icon.png";
+  static const homeIconSvg = "assets/images/home_icon.svg";
+}
